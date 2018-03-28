53f21bef9c3f652ac8b2a6ef4bca435b1b0a42b3
rohankap13 <rohankapoor103@gmail.com>
2018-03-27 23:52:42 -0400

    Created unit tests for account creation

---
+++ UniversityRentingApplication/src/unitTestCases/TestAccountCreation.java
@@ -0,0 +1,46 @@
+package unitTestCases;
+import api.LoginController;
+import api.SignUpController;
+import dao.CustomerDao;
+import dao.DAO;
+import model.Customer;
+import service.CustomerService;
+import utilities.ConnectionData;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class TestAccountCreation {
+	
+	@Test
+	public void returnExpectedCustomerObject() {
+		
+		int id = 4;
+		String fname = "rohan";
+		String lname = "kapoor";
+		String email = "r@uncc.edu";
+		String password = "pass";
+		String phone = "123456";
+		
+		Customer c = new Customer(id, fname, lname, email, password, phone);
+		assertEquals(id, c.getID());
+		assertEquals(fname, c.getFirstName());
+		assertEquals(lname, c.getLastName());
+		assertEquals(email, c.getEmail());
+		assertEquals(password, c.getPassword());
+		assertEquals(phone, c.getPhoneNumber());
+	}
+	
+	@Test
+	public void returnWebPageAction() {
+		DAO d = new DAO();
+		SignUpController lc = new SignUpController(d);
+		String expectedPageToBeOn = "signUp.do";
+		String result = lc.getName();
+		assertEquals(expectedPageToBeOn, result);
+	}
+	
+	
+}
